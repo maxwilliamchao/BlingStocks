180d176371180d137a5aaaa411befc294bc1caab
samuelboediono <samuel_boediono@hotmail.com>
2017-11-29 14:59:34 -0800

    updated serverjs

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
-// ServerJS
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-var hello;
+// Serve up static assets (usually on heroku)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/BlingStocks",
+  {
+    useMongoClient: true
+  }
+);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
