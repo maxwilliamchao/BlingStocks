e3e0823011ed1e71f9480c177e63adaa3786d845
maxwilliamchao <maxwilliamchao@gmail.com>
2017-11-30 14:07:01 -0800

    added a rendering function call

---
+++ client/src/pages/landing.js
@@ -0,0 +1,19 @@
+render() {
+    return (
+      <div>
+        <Searchbar>
+        
+        </Searchbar>
+        <div className="container">
+          <Searchresults  
+          //   topic = {this.state.topic}
+          //   startYr = {this.state.startYr}
+          //   endYr = {this.state.endYr}
+          //   handleFormSubmit={this.handleFormSubmit}
+          //   handleInputChange={this.handleInputChange}
+          />
+          <Searchresults results={this.state.results} />
+        </div>
+      </div>
+    )
+  }
