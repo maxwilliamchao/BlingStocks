25ba9de7a63759cc4e24ae3c6a0164d52a5da6a3
unknown <brandon.geeman.lee@gmail.com>
2017-12-01 12:30:46 -0800

    adding new files and folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import SavedStockCard from './components/SavedStocks'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Login}/>
+        <Route exact path ='/search' component={Search} />
+        <Route exact path = '/search' component={SavedStocks} />
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
