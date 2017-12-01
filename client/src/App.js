a65f270a9d03241ca2016002a81f6e90dca2986f
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-12-01 13:33:21 -0800

    bla bla bla

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import SavedStockCard from './components/SavedStocks'
-
+// import SavedStockCard from './components/SavedStocks'
+import Header from './components/Header'
+import ApplicationLanding from './pages/ApplicationLanding'
+import Dashboard from './pages/Dashboard'
 const App = () =>
   <Router>
     <div>
       <Header />
       <Switch>
-        <Route exact path='/' component={Login}/>
-        <Route exact path ='/search' component={Search} />
-        <Route exact path = '/search' component={SavedStocks} />
+        <Route exact path='/' component={ApplicationLanding}/>
+        <Route exact path ='/dashboard' component={Dashboard} />
+        {/* <Route exact path = '/search' component={SavedStocks} /> */}
       </Switch>
     </div>
   </Router>;
