385d6030472ebbd0cb80a1d560aec7d4e9488823
unknown <brandon.geeman.lee@gmail.com>
2017-12-04 10:20:53 -0800

    deleted faux models

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,14 +3,14 @@ const db = require('../models');
 module.exports = {
 
   create: function(req, res) {
-    db.Stock
+    db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
 
   findAll: function(req, res) {
-    db.Stock
+    db.User
       .find(req.query)
       .sort({date: -1})
       .then(dbModel => res.json(dbModel))
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   remove: function(req, res) {
-    db.Stock
+    db.User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  Stock: require('./Stock'),
-};
