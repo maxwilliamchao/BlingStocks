0826f406c07c81e1350f3d5fe21cba7ef2a89832
unknown <brandon.geeman.lee@gmail.com>
2017-12-03 16:30:58 -0800

    adding controllers

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,6 +2,13 @@ const db = require('../models');
 
 module.exports = {
 
+  create: function(req, res) {
+    db.Stock
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+
   findAll: function(req, res) {
     db.Stock
       .find(req.query)
